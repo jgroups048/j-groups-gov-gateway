68ca699815326104eb167515202e367c4170713a
jgroups048 <jhavicky048@gmail.com>
2025-05-19 20:19:09 +0530

    Made updates in Traei

--- src/data/carouselImages.ts
+++ src/data/carouselImages.ts
@@ -1,14 +1,14 @@
 export const carouselImages = [
   {
-    src: '/images/digital-seva-portal.svg',
-    alt: 'Digital Seva Portal Banner'
+    src: '/images/digital-seva-portal.jpg',
+    alt: 'Digital Seva Portal with government emblem'
   },
   {
-    src: '/images/digital-india-logo.svg',
-    alt: 'Digital India Logo'
+    src: '/images/digital-india-logo.jpg',
+    alt: 'Digital India - Power To Empower'
   },
   {
-    src: '/images/digital-india-6-years.svg',
-    alt: 'Digital India 6 Years Celebration'
+    src: '/images/digital-india-6-years.jpg',
+    alt: 'Celebrating 6 Years of Digital India'
   }
 ];
